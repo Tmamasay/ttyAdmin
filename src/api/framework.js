85cafe65bf9a53dd9c80bc80da14271a42e605c1
chengxu <1056421380@qq.com>
2020-08-21 17:54:17 +0800

    建仓

---
+++ src/api/framework.js
@@ -0,0 +1,416 @@
+import request from '@/utils/request'
+// 创建部门
+export function create(data) {
+  return request({
+    url: '/kl-customer-biz/department/create',
+    method: 'post',
+    data
+  })
+}
+// 快速验证本层下是否有相同名称的部门
+export function validata(data) {
+  return request({
+    url: '/kl-customer-biz/department/validata',
+    method: 'post',
+    data
+  })
+}
+// 获取企业所有部门
+export function getAllDepartment(data) {
+  return request({
+    url: '/kl-customer-biz/department/getAllDepartment',
+    method: 'post',
+    data
+  })
+}
+// 删除部门
+export function del(data) {
+  return request({
+    url: '/kl-customer-biz/department/del',
+    method: 'post',
+    data
+  })
+}
+// 更新部门
+export function udpate(data) {
+  return request({
+    url: '/kl-customer-biz/department/udpate',
+    method: 'post',
+    data
+  })
+}
+// 创建部门成员
+export function cycreate(data) {
+  return request({
+    url: '/kl-customer-biz/user/create',
+    method: 'post',
+    data
+  })
+}
+// 获取部门下对应的成员
+export function getUsersByDepartment(data) {
+  return request({
+    url: '/kl-customer-biz/user/getUsersByDepartment',
+    method: 'post',
+    data
+  })
+}
+// 删除部门成员
+export function cydel(data) {
+  return request({
+    url: '/kl-customer-biz/user/del',
+    method: 'post',
+    data
+  })
+}
+// 更新成员
+export function cyudpate(data) {
+  return request({
+    url: '/kl-customer-biz/user/udpate',
+    method: 'post',
+    data
+  })
+}
+// 快速验证系统中是否存在注册电话对应的成员
+export function cyvalidata(data) {
+  return request({
+    url: '/kl-customer-biz/user/validata',
+    method: 'post',
+    data
+  })
+}
+/* ============坐席管理==============*/
+// 快速验证系统中是否存在注册电话对应的成员
+export function addSeat(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSeatManage/addSeat',
+    method: 'post',
+    data
+  })
+}
+// 删除坐席
+export function delSeat(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSeatManage/delSeat',
+    method: 'post',
+    data
+  })
+}
+// 查询坐席列表分页
+export function findPage(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSeatManage/findPage',
+    method: 'post',
+    data
+  })
+}
+// 修改坐席
+export function hwupdateSeat(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSeatManage/updateSeat',
+    method: 'post',
+    data
+  })
+}
+// 验证坐席
+export function validate(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSeatManage/validate',
+    method: 'post',
+    data
+  })
+}
+// ===========云呼叫===========
+// 验证坐席
+export function callInside(data) {
+  return request({
+    url: '/kl-call-boss-biz/call-out/callInside',
+    method: 'post',
+    data
+  })
+}
+// ===========云呼叫===========
+// 查询自己话单分页记录
+export function findUserCallLogPage(data) {
+  return request({
+    url: '/kl-call-boss-biz/call-out/findUserCallLogPage',
+    method: 'post',
+    data
+  })
+}
+// ===========话单管理===========
+// 通过用户id查询话单记录
+export function findCallLogPage(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSeatManage/findCallLogPage',
+    method: 'post',
+    data
+  })
+}
+
+// 用户登录
+export function userLogin(data) {
+  return request({
+    url: '/th-manager-biz/login/userLogin',
+    method: 'post',
+    data
+  })
+}
+// 查询当前登录用户的菜单
+export function getMyMenus(data) {
+  return request({
+    url: '/th-manager-biz/user-menu/getMyMenus',
+    method: 'post',
+    data
+  })
+}
+// 企业全部角色下拉列表
+export function selectRoleList(data) {
+  return request({
+    url: '/kl-auth-biz/rolePowerManager/selectRoleList',
+    method: 'post',
+    data
+  })
+}
+// 账号修改密码
+export function updatePassword(data) {
+  return request({
+    url: '/kl-auth-biz/login/updatePassword',
+    method: 'post',
+    data
+  })
+}
+// 获取对应的成员
+export function getUsersById(data) {
+  return request({
+    url: '/kl-customer-biz/user/getUsersById',
+    method: 'post',
+    data
+  })
+}
+// 重新发送验证短信
+export function resendVerifyCode(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSeatManage/resendVerifyCode',
+    method: 'post',
+    data
+  })
+}
+// 客户账单分页查询
+export function getPage(data) {
+  return request({
+    url: '/kl-customer-biz/businessInfo/getPage',
+    method: 'post',
+    data
+  })
+}
+// 用户注销接口
+export function loginOut(data) {
+  return request({
+    url: '/kl-auth-biz/login/loginOut',
+    method: 'post',
+    data
+  })
+}
+/* 首页*/
+// 近七天通话详情走势
+export function selectLatelyTrend(data) {
+  return request({
+    url: '/kl-call-boss-biz/homePage/selectLatelyTrend',
+    method: 'post',
+    data
+  })
+}
+// 当日通话情况
+export function findCallCensusNum(data) {
+  return request({
+    url: '/kl-call-boss-biz/homePage/findCallCensusNum',
+    method: 'post',
+    data
+  })
+}
+// 近十条有效话单
+export function selectEffectiveCallLog(data) {
+  return request({
+    url: '/kl-call-boss-biz/homePage/selectEffectiveCallLog',
+    method: 'post',
+    data
+  })
+}
+/* 管理者首页*/
+// 近七天通话详情走势
+export function selectLatelyCompanyData(data) {
+  return request({
+    url: '/kl-call-boss-biz/homePage/manage/selectLatelyCompanyData',
+    method: 'post',
+    data
+  })
+}
+// 当日通话情况
+export function manageFindCallCensusNum(data) {
+  return request({
+    url: '/kl-call-boss-biz/homePage/manage/findCallCensusNum',
+    method: 'post',
+    data
+  })
+}
+// 近十条有效话单
+export function selectYesterdayTop10(data) {
+  return request({
+    url: '/kl-call-boss-biz/homePage/manage/selectTodayAnalysis',
+    method: 'post',
+    data
+  })
+}
+// 消费余额
+export function getUsable(data) {
+  return request({
+    url: '/kl-customer-biz/account/getUsable',
+    method: 'post',
+    data
+  })
+}
+// 重置密码
+export function resetUserPassword(data) {
+  return request({
+    url: '/kl-auth-biz/login/resetUserPassword',
+    method: 'post',
+    data
+  })
+}
+// 上传电话
+export function importTelLibrary(data) {
+  return request({
+    url: '/kl-customer-biz/excel/importTelLibrary',
+    method: 'post',
+    data
+  })
+}
+// 分页查询导入记录
+export function selectPageUploadLog(data) {
+  return request({
+    url: '/kl-customer-biz/excel/selectPageUploadLog',
+    method: 'post',
+    data
+  })
+}
+// 当前企业下客户资源分页查询
+export function getPageCustomerResources(data) {
+  return request({
+    url: '/kl-customer-biz/customerLibrary/getPageCustomerResources',
+    method: 'post',
+    data
+  })
+}
+// 当前用户下客户资源分页查询(私海)
+export function getPageResourcesByUser(data) {
+  return request({
+    url: '/kl-customer-biz/customerLibrary/getPageResourcesByUser',
+    method: 'post',
+    data
+  })
+}
+// 当前企业新增短信签名
+export function addSmsSign(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSwitch/addSmsSign',
+    method: 'post',
+    data
+  })
+}
+// 当前企业下所有短信签名
+export function selectSignList(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSwitch/selectSignList',
+    method: 'post',
+    data
+  })
+}
+// 当前企业新增短信模板
+export function addSmsTemple(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSwitch/addSmsTemple',
+    method: 'post',
+    data
+  })
+}
+// 当前企业下所有短信模板
+export function selectSmsTempleList(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSwitch/selectSmsTempleList',
+    method: 'post',
+    data
+  })
+}
+// 删除当前企业短信模板
+export function deleteSmsTemple(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSwitch/deleteSmsTemple',
+    method: 'post',
+    data
+  })
+}
+// 当前企业下客户挂机短信开关 更改
+export function updateCustomerSmsStatus(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSwitch/updateCustomerSmsStatus',
+    method: 'post',
+    data
+  })
+}
+// 个人话单短信发送记录
+export function selectSmsLogPage(data) {
+  return request({
+    url: '/kl-call-boss-biz/sendSmsManager/selectSmsLogPage',
+    method: 'post',
+    data
+  })
+}
+// 当前企业下客户掩盖电话开关 更改
+export function updateCustomerMaskStatus(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSwitch/updateCustomerMaskStatus',
+    method: 'post',
+    data
+  })
+}
+// 获取客户预警数据
+export function getByCustomer(data) {
+  return request({
+    url: '/kl-customer-biz/balanceWarning/getByCustomer',
+    method: 'post',
+    data
+  })
+}
+// 预警开关
+export function onOffWarning(data) {
+  return request({
+    url: '/kl-customer-biz/balanceWarning/onOffWarning',
+    method: 'post',
+    data
+  })
+}
+// 预警余额设置
+export function setBalance(data) {
+  return request({
+    url: '/kl-customer-biz/balanceWarning/setBalance',
+    method: 'post',
+    data
+  })
+}
+// 删除签名
+export function delSmsSign(data) {
+  return request({
+    url: '/kl-call-boss-biz/customerSwitch/delSmsSign',
+    method: 'post',
+    data
+  })
+}
+export function getDepartmentIdsByUid(data) {
+  return request({
+    url: '/kl-customer-biz/department/getDepartmentIdsByUid',
+    method: 'post',
+    data
+  })
+}
+
---
+++ src/views/number/index.vue
@@ -0,0 +1,574 @@
+<template>
+  <div class="dashboard-container">
+
+    <div class="chart_box shaowAll">
+      <div class="heardNum">
+        <h3 class="Ptitle">号码库上传</h3>
+        <p class="tips">请按模板填写相关信息,未指定用户将上传至公共号码库,上传后失败记录文件有效时间为一天</p>
+      <!-- <a class="downLin2" href="https://htk-call.oss-cn-beijing.aliyuncs.com/excle/template/%E5%AE%A2%E6%88%B7%E4%BF%A1%E6%81%AF%E5%AF%BC%E5%85%A5.xlsx">模板下载</a> -->
+      </div>
+      <div id="myChart" style="width:100%; ">
+        <el-form ref="form" label-position="left" :model="queryUp" label-width="110px">
+          <el-form-item label="模板下载">
+            <a class="downLin2" href="https://htk-call.oss-cn-beijing.aliyuncs.com/excle/template/%E5%AE%A2%E6%88%B7%E4%BF%A1%E6%81%AF%E5%AF%BC%E5%85%A5.xlsx">下载</a>
+          </el-form-item>
+          <el-form-item label="号码掩盖">
+            <el-switch v-model="queryUp.delivery" @change="changeStatus" />
+          </el-form-item>
+          <el-form-item label="选择部门">
+            <el-cascader
+              v-model="queryUp.bumen"
+              :options="bumenData"
+              :props="{ checkStrictly: true }"
+              style="width:180px"
+              placeholder="请选择部门"
+              @change="handleChange"
+            />
+          </el-form-item>
+          <el-form-item label="选择成员">
+            <el-select v-model="queryUp.user" placeholder="请选择用户" @change="getUser1">
+              <el-option
+                v-for="item in userData"
+                :key="item.uid"
+                :label="item.name"
+                :value="item.uid"
+              />
+            </el-select>
+            <el-button type="danger" size="medium" style="margin-left:5px" @click="pushNull">清空选项</el-button>
+          </el-form-item>
+          <el-form-item label="号码上传">
+            <el-upload
+              class="upload-demo"
+              style="width:30%"
+              drag
+              action
+              accept=".xls, .xlsx"
+              :http-request="uploadFile"
+              :limit="1"
+              :on-change="handlePreview"
+              :on-remove="handleRemove"
+              :auto-upload="true"
+            >
+              <i class="el-icon-upload" />
+              <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+              <!-- <div slot="tip" class="el-upload__tip">只能上传execl文件</div> -->
+            </el-upload>
+          </el-form-item>
+        </el-form>
+
+        <!-- <p class="fonNue">号码掩盖开关：<el-switch v-model="delivery" @change="changeStatus" /></p> -->
+        <!-- <div class="checkPer">
+          <el-cascader
+            v-model="queryUp.bumen"
+            :options="bumenData"
+            :props="{ checkStrictly: true }"
+            style="width:180px"
+            placeholder="请选择部门"
+            @change="handleChange"
+          />
+          <el-select v-model="queryUp.user" placeholder="请选择用户" style="width:180px;margin-left:10px" @change="getUser1">
+            <el-option
+
+              v-for="item in userData"
+              :key="item.uid"
+              :label="item.name"
+              :value="item.uid"
+            />
+          </el-select>
+          <el-button type="danger" size="mini" style="margin-left:5px" @click="pushNull">清空</el-button>
+        </div> -->
+        <!-- <el-upload
+          class="upload-demo"
+          style="margin-left:25px;margin-top:30px;width:30%"
+          drag
+          action
+          accept=".xls, .xlsx"
+          :http-request="uploadFile"
+          :limit="1"
+          :on-change="handlePreview"
+          :on-remove="handleRemove"
+          :auto-upload="true"
+        >
+          <i class="el-icon-upload" />
+          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+          <div slot="tip" class="el-upload__tip">只能上传execl文件</div>
+        </el-upload> -->
+
+      </div>
+      <div class="heardNum">
+        <h3 class="Ptitle">导入历史</h3>
+        <p class="tips">每条记录均可下载预览</p>
+      <!-- <a class="downLin2" href="https://htk-call.oss-cn-beijing.aliyuncs.com/excle/template/%E5%AE%A2%E6%88%B7%E4%BF%A1%E6%81%AF%E5%AF%BC%E5%85%A5.xlsx">模板下载</a> -->
+      </div>
+      <div class="table_box">
+        <el-table
+          ref="zx-list-data-&quot;"
+          v-loading="loading"
+          :data="dataList"
+          tooltip-effect="dark"
+          style="width:95%;margin:10px auto 0px auto;"
+          highlight-current-row
+        >
+          <el-table-column prop="fileName" label="上传文件名" />
+          <el-table-column prop="importCount" label="导入数量" />
+          <el-table-column prop="scount" label="成功数量" />
+          <el-table-column prop="fcount" label="失败数量" />
+          <el-table-column prop="createTime" label="上传时间">
+            <template slot-scope="scope">
+              {{ formatDate(scope.row.createTime) }}
+            </template>
+          </el-table-column>
+          <el-table-column width="150px" prop="createTime" label="失败信息">
+            <template slot-scope="scope">
+              <a v-if="scope.row.resUrl" class="downLin" :href="scope.row.resUrl">下载</a>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div class="block fenye">
+          <el-pagination
+            :current-page="Current"
+            :page-sizes="[10, 20, 30, 50]"
+            :page-size="Size"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+          />
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { importTelLibrary, selectPageUploadLog, getAllDepartment, getUsersByDepartment, updateCustomerMaskStatus } from '@/api/framework'
+// import FileSaver from 'file-saver'
+import { getcustomerId } from '@/utils/auth'
+export default {
+  data() {
+    return {
+      queryUp: {
+        delivery: false,
+        bumen: '',
+        user: ''
+      },
+      delivery: false,
+      userUid: '',
+      userData: [], // 选择用户
+      bumenData: [], // 选择部门
+      uploadData: {},
+      yxhdlist: [], // 十条有效话单
+      days: [], // 日期
+      loading: false,
+      lyUrl: '',
+      dialogVisible: false,
+      dataList: [],
+      Current: 1, // 页码
+      total: 0, // 总数
+      Size: 10, // 一页多少条
+      day: '',
+      hstTime: '',
+      thisTime: '',
+      dialogVisible_ly: false
+      // szData: '', // 前7天日期
+      // day: ''
+    }
+  },
+  mounted() {
+    this.getlist()
+    this.getmoreBumen()
+  },
+  methods: {
+    pushNull() {
+      this.queryUp.bumen = ''
+      this.queryUp.user = ''
+      this.userUid = ''
+      this.userData = []
+    },
+    changeStatus(callback) {
+      console.log(callback)
+      var data = {
+        param: {
+          maskStatus: callback ? 1 : 0
+        }
+      }
+      updateCustomerMaskStatus(data).then(res => {
+        if (res.statusCode === '00000') {
+          // this.isActive = res.data
+
+          // if (+res.data.maskStatus === 0) {
+          //   this.delivery = false
+          // } else if (+res.data.maskStatus === 1) {
+          //   this.delivery = true
+          // }
+        }
+      })
+    },
+    getUser1(e) {
+      console.log(e)
+      var obj = {}
+      obj = this.userData.find((item) => item.uid === e)
+      console.log(obj)
+      this.userUid = obj.uid
+    },
+    // 数据格式化
+    generateBm(item, isParent) {
+      const itemCont = {
+        value: item.id,
+        label: item.name,
+        pid: item.pid
+      }
+      return itemCont
+    },
+    forMartBm(data) {
+      if (!data) return []
+      const forMatList = []
+      data.forEach(item => {
+        const parent = this.generateBm(item, true)
+        let children = null
+        if (item.child.length) {
+          children = this.forMartBm(item.child)
+          // item.child.forEach(child => {
+          //   children.push(this.generateBm(child, false))
+          // })
+        }
+        parent.children = children
+        forMatList.push(parent)
+      })
+      return forMatList
+    },
+    // 获取所有部门
+    async getmoreBumen() {
+      const _this = this
+      var data = {
+        param: {
+          customerId: getcustomerId()
+        }
+      }
+      await getAllDepartment(data).then(res => {
+        if (res.statusCode === '00000') {
+          var data = _this.forMartBm(res.data)
+          _this.bumenData = data
+          console.log('11111111111111')
+          console.log(_this.bumenData)
+        }
+      })
+    },
+    async handleChange(value) {
+      console.log(value)
+      this.queryUp.user = ''
+      var data = {
+        param: {
+          departmentId: value[value.length - 1]
+        }
+      }
+      await getUsersByDepartment(data).then(res => {
+        console.log(res)
+        if (res.statusCode === '00000') {
+          this.userData = res.data
+        }
+      })
+    },
+    getlist() {
+      const _this = this
+      _this.loading = true
+      var data = {
+        param: {
+          pageNum: this.Current,
+          pageSize: this.Size
+        }
+      }
+      selectPageUploadLog(data).then(res => {
+        if (res.statusCode === '00000') {
+          setTimeout(res => {
+            _this.loading = false
+          }, 300)
+          _this.dataList = res.data.records
+          _this.total = +res.data.total
+        } else {
+          _this.loading = false
+        }
+      }).catch(err => {
+        _this.$message.error('暂无数据~')
+        _this.loading = false
+        console.log(err)
+      })
+    },
+    // 分页
+    handleSizeChange(val) {
+      this.Size = val
+      this.getlist()
+    },
+    handleCurrentChange(val) {
+      this.Current = val
+      this.getlist()
+    },
+    // 删除时获取文件信息
+    handleRemove(file) {
+      console.log(file)
+    },
+
+    // 获取上传文件信息
+    handlePreview(file) {
+      const _this = this
+      var formData = new FormData()
+      console.log(file)
+      console.log('-0----------->')
+      formData.append('file', file.raw)
+      formData.append('uid', this.userUid)
+
+      _this.uploadData = formData
+
+      console.log(_this.uploadData)
+    },
+    async uploadFile(e) {
+      if (this.queryUp.bumen && !this.userUid) {
+        this.$message({
+          message: '请选择成员',
+          type: 'warning'
+        })
+        this.$refs.upload.clearFiles()
+        return
+      }
+      await importTelLibrary(this.uploadData).then(res => {
+        if (res.statusCode === '00000') {
+          console.log(res)
+          this.$message({
+            message: '上传成功',
+            type: 'success'
+          })
+          this.$refs.upload.clearFiles()
+          this.getlist()
+        } else {
+          this.$message.error(res.status)
+        }
+      })
+    },
+
+    // 时间戳转换
+    formatDate(value) {
+      const date = new Date(value)
+      const y = date.getFullYear()
+      let MM = date.getMonth() + 1
+      MM = MM < 10 ? ('0' + MM) : MM
+      let d = date.getDate()
+      d = d < 10 ? ('0' + d) : d
+      let h = date.getHours()
+      h = h < 10 ? ('0' + h) : h
+      let m = date.getMinutes()
+      m = m < 10 ? ('0' + m) : m
+      let s = date.getSeconds()
+      s = s < 10 ? ('0' + s) : s
+      return y + '-' + MM + '-' + d + ' ' + h + ':' + m + ':' + s
+    },
+    // 时间戳转换
+    formatDate1(value) {
+      const date = new Date(value)
+      const y = date.getFullYear()
+      let MM = date.getMonth() + 1
+      MM = MM < 10 ? ('0' + MM) : MM
+      let d = date.getDate()
+      d = d < 10 ? ('0' + d) : d
+      // let h = date.getHours()
+      // h = h < 10 ? ('0' + h) : h
+      // let m = date.getMinutes()
+      // m = m < 10 ? ('0' + m) : m
+      // let s = date.getSeconds()
+      // s = s < 10 ? ('0' + s) : s
+      return y + '-' + MM + '-' + d
+    },
+
+    // 下载
+    xiazai(e) {
+      console.log(e)
+      // window.location.href = e
+      // FileSaver.saveAs(e, '录音')
+    },
+    toNum(value) {
+      if (!value) return 0
+      return Math.ceil(value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.heardNum{
+      margin-bottom: 24px;
+    padding: 10px 0;
+    font-size: 0;
+    border-bottom: 1px solid #f5f5f5;
+
+}
+.tips{
+  display: inline-block;
+    font-size: 12px;
+    color: #999;
+        margin-left: 10px;
+}
+.shaowAll{
+  /* box-shadow: 2px 4px 8px 8px rgba(0, 0, 0, 0.05); */
+  box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+ padding: 20px;
+}
+.fenye{
+    display: block;
+    text-align: right;
+    margin-top:20px;
+}
+.Ptitle{
+      font-size: 18px;
+    line-height: 18px;
+    color: #222;
+    font-weight: 700;
+    /* padding: 12px; */
+    display: inline-block;
+}
+#myChart{
+  float:left;
+  background: #fff;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+}
+#myChart2{
+  float:left;
+  background: #fff;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+}
+.chart_box{
+  overflow: hidden;
+}
+.dashboard-container{
+  background: #ffffff;
+  padding:20px;
+  overflow: auto;
+}
+.dashboard-container::-webkit-scrollbar{
+
+   display:none;
+
+}
+.drqk_box{
+  margin-top:50px;
+  clear: both;
+  /* box-shadow: 0px 0px 5px #d2d2d2; */
+  overflow: hidden;
+  padding-bottom:10px;
+}
+.fonNue{
+  font-size: 16px;
+    color: #707070;
+    padding-left:30px;
+}
+.title_box p{
+    font-size: 24px;
+    color: #363636;
+    margin:0;
+  }
+  .title_box .line{
+    display: block;
+    width: 34px;
+    height: 4px;
+    background: #00c48f;
+    margin: 8px 0 30px 0;
+  }
+  .downLin{
+    display: inline-block;
+    padding: 3px 10px;
+    background-color: #00c48f;
+    color: #fff;
+    border-radius: 3px;
+  }
+  .downLin2{
+    /* display: inline-block; */
+    padding: 8px 10px;
+    background-color: #00c48f;
+    color: #fff;
+    font-size: 15px;
+    border-radius: 3px;
+    margin-bottom: 10px;
+  }
+  .title{
+    font-weight: 600;
+    font-size: 18px;
+    margin-top:50px;
+  }
+.drqk_box .content_box{
+  /* width:320px; */
+  width:16.5%;
+  height:100px;
+  background: #fff;
+  float:left;
+  margin-left:122px;
+  box-shadow: 0px 0px 5px #d2d2d2;
+  border-radius: 5px;
+  margin-top:20px;
+}
+.drqk_box .content_box .img{
+  width:90px;
+  height:90px;
+  background: #fc9208;
+  border-radius: 5px;
+  position: relative;
+  top: -40px;
+  left: 20px;
+  box-shadow: 0px 10px 15px #eee;
+  display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: column;
+    float:left;
+}
+.drqk_box .content_box:nth-of-type(3) .img{
+  background: #09b2c7;
+}
+.drqk_box .content_box:nth-of-type(4) .img{
+  background: #e8413d;
+}
+.drqk_box .content_box:nth-of-type(5) .img{
+  background: #4da851;
+}
+.drqk_box .content_box div{
+  float:right;
+  margin-right:20px;
+  text-align: right;
+}
+.drqk_box .content_box div p{
+  color:#9f9f9f;
+  font-size:14px;
+}
+.table_box .title_box{
+  margin-top:50px;
+}
+.icon{
+  cursor: pointer;
+}
+.icon_box .svg-icon:hover{
+  color:#1c6feb;
+}
+audio{
+  outline: none;
+}
+.drqk_box .content_box .img .svg-icon{
+  width:30px;
+  height:30px;
+}
+audio{
+  outline: none;
+}
+.ls{
+  color:#2acc69
+}
+.hs{
+  color:#fb5e5e
+}
+.checkPer{
+  display: flex;
+  padding-top: 15px;
+  padding-left: 25px;
+  width: 350px;
+  justify-content: space-between;
+}
+</style>
